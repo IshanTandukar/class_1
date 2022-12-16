b8f4c9d23139e8874e61b5edd88cb77a58ce1bbf
Ishan Tandukar <077bct019.ishan@sagarmatha.edu.np>
2022-12-16 09:53:39 +0545

    commit

--- class_7.py
+++ class_7.py
@@ -7,18 +7,18 @@
 my_string = "Hello world from new python script"
 #for i in my_string:
  #   print (i)#
- #   if i=="l"  :
+ #   if i == "l"  :
  #       i=i.upper()
 
 #strip
-my_string1="           Hello from new python"
+my_string1 = "           Hello from new python"
 print(my_string1.strip())
 
 
 
 #formatting a string
 a = "Ram Bahadur"
-#b = f"mero nam {a} haina"
+b = f"mero nam {a} haina"
 c = f"mero naam {a} ho {my_string}"
 b = "mero nam {2} ho {2}, {1}, {2}".format(a,my_string,c)
 
@@ -30,8 +30,8 @@ my_string = my_string.replace("Hello","bye")
 print(my_string)
 
 #counting number of string
-my_string2="aaabcdefaaaabababababaababaabaabbba"
-my_string2=my_string2.count("ab")
+my_string2 = "aaabcdefaaaabababababaababaabaabbba"
+my_string2 = my_string2.count("ab")
 print(my_string2)
 
 #enumerate function
